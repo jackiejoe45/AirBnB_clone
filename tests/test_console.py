63f304976ea966cb4e0a8097f19227f02049d545
Ben Charles Abdul <ben.abdul@ashesi.edu.gh>
2023-10-16 21:18:13 +0000

    Added more tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -3,7 +3,7 @@
 Test for console
 '''
 import unittest
-import os
+# import os
 from io import StringIO
 from unittest.mock import patch
 from console import HBNBCommand
@@ -123,6 +123,238 @@ class TestConsole(unittest.TestCase):
             self.consol.do_create("BaseModel")
             self.assertTrue(len(f.getvalue()) > 0)
 
+    def test_all(self):
+        '''Test all'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.do_all("")
+            expected_output = "["
+            self.assertEqual(f.getvalue(), expected_output)
 
+    def test_count(self):
+        '''Test count'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.do_count("")
+            expected_output = "0\n"
+            self.assertEqual(f.getvalue(), expected_output)
+
+    def test_show(self):
+        '''Test show'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.do_show("")
+            expected_output = "** class name missing **\n"
+            self.assertEqual(f.getvalue(), expected_output)
+
+    def test_destroy(self):
+        '''Test destroy'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.do_destroy("")
+            expected_output = "** class name missing **\n"
+            self.assertEqual(f.getvalue(), expected_output)
+
+    def test_update(self):
+        '''Test update'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.do_update("")
+            expected_output = "** class name missing **\n"
+            self.assertEqual(f.getvalue(), expected_output)
+
+        
+    def test_create_with_class(self):
+        '''Test create with class'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.do_create("BaseModel")
+            expected_output = "["
+            self.assertEqual(f.getvalue(), expected_output)
+
+    def test_show_with_class_name(self):
+        '''Test show with class name'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.do_show("BaseModel")
+            expected_output = "** instance id missing **\n"
+            self.assertEqual(f.getvalue(), expected_output)
+
+    def test_create_invalid_class(self):
+        '''Test create with invalid class name'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.do_create("InvalidClassName")
+            expected_output = "** class doesn't exist **\n"
+            self.assertEqual(f.getvalue(), expected_output)
+
+    def test_show_invalid_class(self):
+        '''Test show with invalid class name'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.do_show("InvalidClassName 123")
+            expected_output = "** class doesn't exist **\n"
+            self.assertEqual(f.getvalue(), expected_output)
+
+    def test_show_missing_id(self):
+        '''Test show with missing id'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.do_show("BaseModel")
+            expected_output = "** instance id missing **\n"
+            self.assertEqual(f.getvalue(), expected_output)
+
+    def test_show_invalid_id(self):
+        '''Test show with invalid id'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.do_show("BaseModel 123")
+            expected_output = "** no instance found **\n"
+            self.assertEqual(f.getvalue(), expected_output)
+
+    def test_destroy_invalid_class(self):
+        '''Test destroy with invalid class name'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.do_destroy("InvalidClassName 123")
+            expected_output = "** class doesn't exist **\n"
+            self.assertEqual(f.getvalue(), expected_output)
+
+    def test_destroy_missing_id(self):
+        '''Test destroy with missing id'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.do_destroy("BaseModel")
+            expected_output = "** instance id missing **\n"
+            self.assertEqual(f.getvalue(), expected_output)
+
+    def test_destroy_invalid_id(self):
+        '''Test destroy with invalid id'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.do_destroy("BaseModel 123")
+            expected_output = "** no instance found **\n"
+            self.assertEqual(f.getvalue(), expected_output)
+
+    def test_all_invalid_class(self):
+        '''Test all with invalid class name'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.do_all("InvalidClassName")
+            expected_output = "** class doesn't exist **\n"
+            self.assertEqual(f.getvalue(), expected_output)
+
+    def test_update_invalid_class(self):
+        '''Test update with invalid class name'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.do_update("InvalidClassName 123")
+            expected_output = "** class doesn't exist **\n"
+            self.assertEqual(f.getvalue(), expected_output)
+
+    def test_update_missing_id(self):
+        '''Test update with missing id'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.do_update("BaseModel")
+            expected_output = "** instance id missing **\n"
+            self.assertEqual(f.getvalue(), expected_output)
+
+    def test_update_invalid_id(self):
+        '''Test update with invalid id'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.do_update("BaseModel 123")
+            expected_output = "** no instance found **\n"
+            self.assertEqual(f.getvalue(), expected_output)
+
+    def test_update_missing_attr_name(self):
+        '''Test update with missing attribute name'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.do_update("BaseModel 123")
+            expected_output = "** attribute name missing **\n"
+            self.assertEqual(f.getvalue(), expected_output)
+
+    def test_update_missing_value(self):
+        '''Test update with missing value'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.do_update("BaseModel 123 attr_name")
+            expected_output = "** value missing **\n"
+            self.assertEqual(f.getvalue(), expected_output)
+
+    def test_create_invalid_class(self):
+        '''Test create with invalid class name'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.do_create("InvalidClass")
+            expected_output = "** class doesn't exist **\n"
+            self.assertEqual(f.getvalue(), expected_output)
+
+    def test_show_invalid_class(self):
+        '''Test show with invalid class name'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.do_show("InvalidClass 123")
+            expected_output = "** class doesn't exist **\n"
+            self.assertEqual(f.getvalue(), expected_output)
+
+    def test_show_missing_id(self):
+        '''Test show with missing id'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.do_show("BaseModel")
+            expected_output = "** instance id missing **\n"
+            self.assertEqual(f.getvalue(), expected_output)
+
+    def test_show_invalid_id(self):
+        '''Test show with invalid id'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.do_show("BaseModel 123")
+            expected_output = "** no instance found **\n"
+            self.assertEqual(f.getvalue(), expected_output)
+
+    def test_destroy_invalid_class(self):
+        '''Test destroy with invalid class name'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.do_destroy("InvalidClass 123")
+            expected_output = "** class doesn't exist **\n"
+            self.assertEqual(f.getvalue(), expected_output)
+
+    def test_destroy_missing_id(self):
+        '''Test destroy with missing id'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.do_destroy("BaseModel")
+            expected_output = "** instance id missing **\n"
+            self.assertEqual(f.getvalue(), expected_output)
+
+    def test_destroy_invalid_id(self):
+        '''Test destroy with invalid id'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.do_destroy("BaseModel 123")
+            expected_output = "** no instance found **\n"
+            self.assertEqual(f.getvalue(), expected_output)
+
+    def test_all_invalid_class(self):
+        '''Test all with invalid class name'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.do_all("InvalidClass")
+            expected_output = "** class doesn't exist **\n"
+            self.assertEqual(f.getvalue(), expected_output)
+
+    def test_update_invalid_class(self):
+        '''Test update with invalid class name'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.do_update("InvalidClass 123")
+            expected_output = "** class doesn't exist **\n"
+            self.assertEqual(f.getvalue(), expected_output)
+
+    def test_update_missing_id(self):
+        '''Test update with missing id'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.do_update("BaseModel")
+            expected_output = "** instance id missing **\n"
+            self.assertEqual(f.getvalue(), expected_output)
+
+    def test_update_invalid_id(self):
+        '''Test update with invalid id'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.do_update("BaseModel 123")
+            expected_output = "** no instance found **\n"
+            self.assertEqual(f.getvalue(), expected_output)
+
+    def test_update_missing_attr_name(self):
+        '''Test update with missing attribute name'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.do_update("BaseModel 123")
+            expected_output = "** attribute name missing **\n"
+            self.assertEqual(f.getvalue(), expected_output)
+
+    def test_update_missing_value(self):
+        '''Test update with missing value'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.do_update("BaseModel 123 attr_name")
+            expected_output = "** value missing **\n"
+            self.assertEqual(f.getvalue(), expected_output)
+
+    
 if __name__ == '__main__':
     unittest.main()
